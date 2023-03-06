a6dff0cae940f4065b6014b508add9c214a4ab0e
florian saffaf <florian.saffaf@gmail.com>
2023-03-06 10:01:32 +0100

    changes

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+    require 'db.class.php';
+    $DB = new DB();
+
+    $req =$DB->db->prepare('SELECT * FROM produit');
+    $req -> execute ();
+    var_dump($req->fetchall());
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,9 +14,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="script.js" type="text/javascript"></script>
-    <title>Document</title>
+    <title>Mon producteur bio</title>
 </head>
 <body>
-    
+    <header>
+        <img src="/producteur-bio/images_site/logo_1.png">
+        <h1>PAS BESOIN D'ALLER LOIN POUR BIEN MANGER</h1>
+    </header>
+        <img src="/producteur-bio/images_site/annonce_ouverture.png">
+
+
+
+    <div id="boutique"></div>
 </body>
 </html>
