e900f8775a520be99f5f4f99076738bc2bd1f79e
Airald <airald.73@orange.fr>
2023-03-06 16:24:58 +0100

    ajout de classe CSS

--- form.php
+++ form.php
@@ -1,20 +1,6 @@
 <?php
-// Configuration de la connexion
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','root');
-define('DB_NAME','prodbio');
+include("db.class.php");
 
-try
-{
-    // Connexion � la base
-    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
-}
-catch (PDOException $e)
-{
-	// Echec de la connexion
-    exit("Error: " . $e->getMessage());
-}
 if(TRUE === isset($_POST['submit'])){
     // Identification de TOUT les éléments
         $nom = $_POST['nom'];
@@ -30,41 +16,31 @@ if(TRUE === isset($_POST['submit'])){
         $code_postal_fact = $_POST['code_postal_fact'];
         $ville_fact = $_POST['ville_fact'];
         //var_dump($_POST);
+        $sql = "INSERT INTO user (Nom, Prénom, Email, NumEtRue, ComplementAdresse, CodePostal, Ville, NumMobile, NumEtRueFact, ComplementAdresseFact, CodePostalFact, VilleFact)
+        VALUE (:nom, :prenom, :mail, :num_et_rue, :comp_adresse, :code_postal, :ville, :numero_tel, :num_et_rue_fact, :comp_adresse_fact, :code_postal_fact, :ville_fact)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':nom',$nom,PDO::PARAM_STR);
+        $query->bindParam(':prenom',$prenom,PDO::PARAM_STR);
+        $query->bindParam(':mail',$mail,PDO::PARAM_STR);
+        $query->bindParam(':num_et_rue',$num_et_rue,PDO::PARAM_STR);
+        $query->bindParam(':comp_adresse',$comp_adresse,PDO::PARAM_STR);
+        $query->bindParam(':code_postal',$code_postal,PDO::PARAM_STR);
+        $query->bindParam(':ville',$ville,PDO::PARAM_STR);
+        $query->bindParam(':numero_tel',$numero_tel,PDO::PARAM_STR);
+
+        // Si il y a rien d'écrit dans les champs de facturation alors on execute avec les champs de livraison
         if(strlen($num_et_rue_fact) == 0 && strlen($comp_adresse_fact) == 0 &&  strlen($code_postal_fact) ==  0 && strlen($ville_fact) == 0){
-            $sql = "INSERT INTO user (Nom, Prénom, Email, NumEtRue, ComplementAdresse, CodePostal, Ville, NumMobile, NumEtRueFact, ComplementAdresseFact, CodePostalFact, VilleFact)
-            VALUE (:nom, :prenom, :mail, :num_et_rue, :comp_adresse, :code_postal, :ville, :numero_tel, :num_et_rue_fact, :comp_adresse_fact, :code_postal_fact, :ville_fact)";
-            $query = $dbh->prepare($sql);
-            $query->bindParam(':nom',$nom,PDO::PARAM_STR);
-            $query->bindParam(':prenom',$prenom,PDO::PARAM_STR);
-            $query->bindParam(':mail',$mail,PDO::PARAM_STR);
-            $query->bindParam(':num_et_rue',$num_et_rue,PDO::PARAM_STR);
-            $query->bindParam(':comp_adresse',$comp_adresse,PDO::PARAM_STR);
-            $query->bindParam(':code_postal',$code_postal,PDO::PARAM_STR);
-            $query->bindParam(':ville',$ville,PDO::PARAM_STR);
-            $query->bindParam(':numero_tel',$numero_tel,PDO::PARAM_STR);
             $query->bindParam(':num_et_rue_fact',$num_et_rue,PDO::PARAM_STR);
             $query->bindParam(':comp_adresse_fact',$comp_adresse,PDO::PARAM_STR);
             $query->bindParam(':code_postal_fact',$code_postal,PDO::PARAM_STR);
             $query->bindParam(':ville_fact',$ville,PDO::PARAM_STR);
-            $query->execute();
         }else{
-            $sql = "INSERT INTO user (Nom, Prénom, Email, NumEtRue, ComplementAdresse, CodePostal, Ville, NumMobile, NumEtRueFact, ComplementAdresseFact, CodePostalFact, VilleFact)
-            VALUE (:nom, :prenom, :mail, :num_et_rue, :comp_adresse, :code_postal, :ville, :numero_tel, :num_et_rue_fact, :comp_adresse_fact, :code_postal_fact, :ville_fact)";
-            $query = $dbh->prepare($sql);
-            $query->bindParam(':nom',$nom,PDO::PARAM_STR);
-            $query->bindParam(':prenom',$prenom,PDO::PARAM_STR);
-            $query->bindParam(':mail',$mail,PDO::PARAM_STR);
-            $query->bindParam(':num_et_rue',$num_et_rue,PDO::PARAM_STR);
-            $query->bindParam(':comp_adresse',$comp_adresse,PDO::PARAM_STR);
-            $query->bindParam(':code_postal',$code_postal,PDO::PARAM_STR);
-            $query->bindParam(':ville',$ville,PDO::PARAM_STR);
-            $query->bindParam(':numero_tel',$numero_tel,PDO::PARAM_STR);
             $query->bindParam(':num_et_rue_fact',$num_et_rue_fact,PDO::PARAM_STR);
             $query->bindParam(':comp_adresse_fact',$comp_adresse_fact,PDO::PARAM_STR);
             $query->bindParam(':code_postal_fact',$code_postal_fact,PDO::PARAM_STR);
             $query->bindParam(':ville_fact',$ville_fact,PDO::PARAM_STR);
-            $query->execute();
         }
+        $query->execute();
 }
 ?>
 
@@ -75,77 +51,100 @@ if(TRUE === isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="http://localhost/producteur-bio/style.css">
+    <script src="script.js" type="text/javasccript"></script>
     <title>Mon producteur bio | formulaire de précommande</title>
 </head>
 <body>
-    <img src="./images_site/logo_1.png" alt="logo-mon-producteur-bio">
+    <div class="conteneur-logo">
+    <img class="logo" src="./images_site/logo_1.png" alt="logo-mon-producteur-bio">
+    </div>
+    <div class="conteneur-titre">
+    <h1 class="titre">Formulaire de précommande</h1>
+    </div>
+    
+    <input type="button" name="retour" class="btnOrange" value="Continuer mes achats">
+    <h2 class="label-grand">Récapitulatif du panier</h2>
+    <!-- Panier -->
+    <!-- Début du formulaire -->
     <form action="form.php" method="post">
         <!-- Nom et prénom sont sur la même ligne -->
-        <div>
-            <div class="">
-                <label>Nom *</label>
-                <input type="text" name="nom" required>
+        <div class="conteneur-inline">
+            <div class="div-inline">
+                <label class="label-grand">Nom *</label><br>
+                <input class="input-form" type="text" name="nom" required>
             </div>
-                <div class="">
-                    <label>Prénom *</label>
-                    <input type="text" name="prenom" required>
+                <div class="div-inline">
+                    <label class="label-grand">Prénom *</label><br>
+                    <input class="input-form" type="text" name="prenom" required>
 				</div>
         </div>
             <div class="">
-                <label>Adresse mail *</label>
-                <input type="text" name="mail" required>
+                <label class="label-grand">Adresse mail *</label><br>
+                <input class="input-form" type="text" name="mail" required>
             </div>
             <div class="">
-                <label>Adresse de livraison *</label>
+                <h3 class="label-grand">Adresse de livraison *</h3>
 
-                <input type="text" name="num_et_rue" required>
-                <label>N° et Rue</label>
+                <input class="input-form" type="text" name="num_et_rue" required><br>
+                <label class="label-petit">N° et Rue</label><br>
 
-                <input type="text" name="comp_adresse" required>
-                <label>Complément d'adresse</label>
+                <input class="input-form" type="text" name="comp_adresse" required><br>
+                <label class="label-petit">Complément d'adresse</label><br>
 
-                <input type="text" name="code_postal" required>
-                <label>Code postal</label>
+                <input class="input-form" type="text" name="code_postal" required><br>
+                <label class="label-petit">Code postal</label><br>
 
-                <input type="text" name="ville" required>
-                <label>Ville</label>
+                <input class="input-form" type="text" name="ville" required><br>
+                <label class="label-petit">Ville</label><br>
             </div>
             <div class="">
-                <label>Numéro de téléphone *</label>
-                <input type="text" name="numero_tel" required>
+                <label class="label-grand">Numéro de téléphone *</label><br>
+                <input class="input-form" type="text" name="numero_tel" required>
             </div>
             <div class="">
-                <label>Adresse de Facturation (si différente de celle de livraison)</label>
+                <h3 class="label-grand">Adresse de Facturation (si différente de celle de livraison)</h3>
 
-                <input type="text" name="num_et_rue_fact">
-                <label>N° et Rue</label>
+                <input class="input-form" type="text" name="num_et_rue_fact"><br>
+                <label class="label-petit">N° et Rue</label><br>
 
-                <input type="text" name="comp_adresse_fact">
-                <label>Complément d'adresse</label>
+                <input class="input-form" type="text" name="comp_adresse_fact"><br>
+                <label class="label-petit">Complément d'adresse</label><br>
 
-                <input type="text" name="code_postal_fact">
-                <label>Code postal</label>
+                <input class="input-form" type="text" name="code_postal_fact"><br>
+                <label class="label-petit">Code postal</label><br>
 
-                <input type="text" name="ville_fact">
-                <label>Ville</label>
+                <input class="input-form" type="text" name="ville_fact"><br>
+                <label class="label-petit">Ville</label><br>
             </div>
             <div>
-                <input type="checkbox" name="visa">
+                <h3 class="label-grand">Mode de paiement</h3>
+                
+                <div>
+                <input type="radio" name="paiement" id="visa" required>
                 <label>Visa</label>
+                </div>
 
-                <input type="checkbox" name="masterCard">
+                <div>
+                <input type="radio" name="paiement" id="masterCard" required>
                 <label>MasterCard</label>
+                </div>
 
-                <input type="checkbox" name="paypal">
+                <div>
+                <input type="radio" name="paiement" id="paypal" required>
                 <label>Paypal</label>
+                </div>
             </div>
-            <div>
+            <div class="cgu-cgv">
                 <input type="checkbox" name="cgu_cgv" required>
-                <label>En cochant cette vous reconnaissez avec pris connaissance des Conditions Générales d’Utilisation et Conditions Générales de Ventes *</label>
+                <label>En cochant cette vous reconnaissez avec pris connaissance des <a href="#"> Conditions Générales d’Utilisation</a> et <a href="#"> Conditions Générales de Ventes</a> *</label>
             </div>
-            <p>*= Mention obligatiore</p>
-            <div>
-                <input type="submit" name="submit" value="Valider">
+            <p style="font-family: 'Montserrat';">*= Mention obligatiore</p>
+            <div class="conteneur-valider">
+                <input class="btnOrange" type="submit" name="submit" value="Valider">
             </div>
     </form>
 </body>
