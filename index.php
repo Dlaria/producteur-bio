a7323f6d606d88233728ab09521f466cdb64cf39
Airald <airald.73@orange.fr>
2023-03-10 16:45:33 +0100

    Style et maj de l'index

--- index.php
+++ index.php
@@ -8,16 +8,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="script.js" type="text/javascript"></script>
     <title>Mon producteur bio</title>
 </head>
 <body>
     <header>
-        <img src="/producteur-bio/images_site/logo_1.png">
-        <h1>PAS BESOIN D'ALLER LOIN POUR BIEN MANGER</h1>
+        <div class="conteneur-logo">
+            <img class="logo" src="/producteur-bio/images_site/logo_1.png">
+        </div>
+        <div class="conteneur-titre">
+        <h1 class="titre-index">PAS BESOIN D'ALLER LOIN POUR BIEN MANGER</h1>
+        </div>
     </header>
-        <img src="/producteur-bio/images_site/annonce_ouverture.png">
+
+        <img class="annonce" src="/producteur-bio/images_site/annonce_ouverture.png" alt="annonce_ouverture">
     
     <div class="présentation">
         <h2>NOTRE MISSION</h2>
@@ -25,44 +33,38 @@
         <br><br>
             C’est pourquoi nous avons décidé de rendre tout cela possible, vous pourrez directement filtrer sur votre région pour voir tous les produits fabriqués proche de chez vous avec en prime la photo du producteur qui vous permettra de mettre un visage derrière tout ce travail.
         </p>
-        <img src="/producteur-bio/images_site/entreprise.jpg">
+        <div class="feuilles"></div>
+        <img class="imgPrésentation" src="/producteur-bio/images_site/entreprise.jpg">
+    </div>
+    <div class="présentation-produit">
         <h2>NOS PRODUITS</h2>
         <p>Sélectionnez les quantités que vous souhaitez précommander et cliquez sur “Je précommande” pour valider.
-<br><br>
-Nous    effectuons la livraison à domicile pour le moment, la livraison en relais colis arrive bientôt ! 
-<br>
-Nous    appliquons des frais de port fixe de 4€ pour la livraison, frais de port offert à partir de 40€ d’achat.
-        </p>
+    <br><br>
+    Nous    effectuons la livraison à domicile pour le moment, la livraison en relais colis arrive bientôt ! 
+    <br>
+    Nous    appliquons des frais de port fixe de 4€ pour la livraison, frais de port offert à partir de 40€ d’achat.
+            </p>
     </div>
 
-        <div class="home">
-    <div class="container">
-         
-    <div class="row">
-        <div class="wrap">
-            <?php $produits = $DB->query('SELECT *FROM produit'); ?>
-            <?php foreach ($produits as $produit):?>
-                <div class="box">
-                    <div class="produit">
-                        <a href="#">
-                            <img src="<?php echo $produit->SrcImage; ?>" alt="">
-                        </a>
-                        <div class="description">
-                            
-                            <?php echo $produit->Nom; ?>
-                            <?php echo $produit->Origine; ?>
-                            <p class="price"><?php echo number_format($produit->Prix,2,',',''); ?> </p>
-                        </div>
-                        <a class="add" href="./form.php?id=<?php echo $produit->id; ?>">
-                        add</a>
+
+    <?php $produits = $DB->query('SELECT *FROM produit'); ?>
+    <?php foreach ($produits as $produit):?>
+        
+            <div class="produit">
+                    <img src="<?php echo $produit->SrcImage; ?>" alt="">
+                <div class="description">
+                    <p class="nom-produit"><?php echo $produit->Nom; ?></p>
+                    <p>Origine : <br><?php echo $produit->Origine; ?></p>
                 </div>
-            <?php endforeach ?>
-        </div>
-    </div>
-    </div>
-</div>
+                <p class="price"><?php echo number_format($produit->Prix,2,',',''); ?> €</p>
+                <a class="add" href="./form.php?id=<?php echo $produit->id; ?>">add</a>
+            </div>
+    <?php endforeach ?>
+
 <footer>
+    <div class="logo">
     <img src="/producteur-bio/images_site/logo_1.png">
+    </div>
     <div>
         <h3>Mon Productuer Bio</h3>
         <p>Qui sommes-nous ?</p>
@@ -82,7 +84,7 @@ Nous    appliquons des frais de port fixe de 4€ pour la livraison, frais de po
         <p>CGV/CGU</p>
         <p>Politique de confidentialité</p>
     </div>
-    <div>
+    <div class="reseaux">
         <h3>Nos réseaux sociaux</h3>
         <img src="/producteur-bio/images_site/icon_facebook.png">
         <img src="/producteur-bio/images_site/icon_instagram.png">
